337539266497edf974c02ab8fcca849bae83efac
Vladislav Mukhamatnurov <v.mukhamatnurov@tinkoff.ru>
2019-10-07 17:05:55 +0500

    Updated tests

--- tests/unit/test_markers_presence.py
+++ tests/unit/test_markers_presence.py
@@ -100,7 +100,6 @@ class TestMarkersPresencePositive:
             [
                 "Markers presence:*",
                 f"*{Options.STAGING}*Stage project with markers based on directories names",
-                "*in 'tests' folder",
                 f"*{Options.ASSERT_STEPS}*Represent assertion comparisons with Allure steps",
                 f"*{Options.BDD_TITLES}*Set Allure titles for BDD test scenarios",
                 f"*{Options.BDD_FORMAT}*Show not classified functions usage and items without",
